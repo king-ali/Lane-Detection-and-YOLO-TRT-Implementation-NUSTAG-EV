da204f24962f1f749c349e8cc205e86633ecef28
Ali Akram <30436017+king-ali@users.noreply.github.com>
2022-05-30 01:23:26 +0500

    Add files via upload

---
+++ camera/test_cam.py
@@ -0,0 +1,30 @@
+import cv2
+
+import time
+if __name__ == '__main__':
+    
+    #cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    #cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    cam = cv2.VideoCapture(0)
+    i = 0
+    num_frames = 10000
+    start = time.time()
+        
+    count = 0
+    for i in range(num_frames):
+        ret, frame = cam.read()
+    	# Display the resulting frame
+        cv2.imshow('frame',frame)
+        cv2.waitKey(1)
+        print(frame.shape)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    break
+        #cv2.imwritre("./testlane/"+str(count)+".png",frame)
+        #count = count + 1
+
+    end = time.time()
+    seconds = end - start
+    fps  = num_frames / seconds
+    print("Estimated frames per second : {0}".format(fps))
+    cam.release()    
---
+++ camera/test_cam_ROS.py
@@ -0,0 +1,30 @@
+import cv2
+#import cv_bridge
+from sensor_msgs.msg import Image
+import rospy
+import numpy as np
+
+class CameraTest(object):
+    def __init__(self):
+        self.camera_topic = '/usb_cam/image_raw'
+        self.image_sub = rospy.Subscriber( self.camera_topic, Image, self.image_callback)
+        self.image = None
+        
+
+    def image_callback(self,msg):
+        self.image = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+        #print(self.image.shape)
+        cv2.imshow('asdf',self.image)
+        cv2.waitKey(1)
+
+        
+            
+if __name__ == '__main__':
+    try:
+        rospy.init_node('cam_reader', anonymous=True)
+        
+        lane = CameraTest()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+   
