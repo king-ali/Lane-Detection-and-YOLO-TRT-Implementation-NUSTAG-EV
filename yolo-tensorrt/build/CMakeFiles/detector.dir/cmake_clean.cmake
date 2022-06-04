4245fcc4922caadd70e19503b0f858d98cc9305c
Ali Akram <30436017+king-ali@users.noreply.github.com>
2022-06-05 01:54:24 +0500

    Add files via upload

---
+++ yolo-tensorrt/build/CMakeFiles/detector.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detector.dir/modules/calibrator.cpp.o"
+  "CMakeFiles/detector.dir/modules/class_detector.cpp.o"
+  "CMakeFiles/detector.dir/modules/ds_image.cpp.o"
+  "CMakeFiles/detector.dir/modules/plugin_factory.cpp.o"
+  "CMakeFiles/detector.dir/modules/trt_utils.cpp.o"
+  "CMakeFiles/detector.dir/modules/yolo.cpp.o"
+  "CMakeFiles/detector.dir/modules/yolo_config_parser.cpp.o"
+  "CMakeFiles/detector.dir/modules/yoloplugin_lib.cpp.o"
+  "CMakeFiles/detector.dir/modules/yolov2.cpp.o"
+  "CMakeFiles/detector.dir/modules/yolov3.cpp.o"
+  "CMakeFiles/detector.dir/modules/yolov4.cpp.o"
+  "CMakeFiles/detector.dir/modules/yolov5.cpp.o"
+  "CMakeFiles/detector.dir/modules/chunk.cu.o"
+  "CMakeFiles/detector.dir/modules/detect.cu.o"
+  "CMakeFiles/detector.dir/modules/hardswish.cu.o"
+  "CMakeFiles/detector.dir/modules/kernel.cu.o"
+  "CMakeFiles/detector.dir/modules/mish.cu.o"
+  "CMakeFiles/detector.dir/cmake_device_link.o"
+  "libdetector.pdb"
+  "libdetector.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
