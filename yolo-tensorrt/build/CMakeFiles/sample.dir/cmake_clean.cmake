f445dd572dc95495275756572f8b3ce2916f899b
Ali Akram <30436017+king-ali@users.noreply.github.com>
2022-06-05 01:58:08 +0500

    Add files via upload

---
+++ yolo-tensorrt/build/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/samples/sample_detector.cpp.o"
+  "devel/lib/yolo-trt/sample.pdb"
+  "devel/lib/yolo-trt/sample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
