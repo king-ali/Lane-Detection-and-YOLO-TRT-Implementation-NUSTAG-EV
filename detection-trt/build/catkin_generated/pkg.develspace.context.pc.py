832b64c7ac18d39033b9cf168682104261da1126
Ali Akram <30436017+king-ali@users.noreply.github.com>
2022-06-06 00:07:50 +0500

    Add files via upload

---
+++ detection-trt/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "yolo-trt"
+PROJECT_SPACE_DIR = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/build/devel"
+PROJECT_VERSION = "0.0.0"
