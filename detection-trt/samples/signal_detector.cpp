64d80bde67e99797be2df104fa4d94992ab865ca
Ali Akram <30436017+king-ali@users.noreply.github.com>
2022-06-06 02:09:00 +0500

    Update signal_detector.cpp

--- detection-trt/samples/signal_detector.cpp
+++ detection-trt/samples/signal_detector.cpp
@@ -14,26 +14,7 @@
 
 using namespace cv;
 
-//std::mutex m_buf;
 
-
-
-/*
-void img0_callback(const sensor_msgs::ImageConstPtr &img_msg)
-{
-
-	std::cout<<"Image callback"<<std::endl;
-    //m_buf.lock();
-    img0_buf.push(img_msg);
-	namedWindow("image",  WINDOW_NORMAL);
-	image0 = getImageFromMsg(img0_buf.front());
-	img0_buf.pop();
-	//imshow("image", image0);
-	batch_img.push_back(image0);
-	
-	//m_buf.unlock();
-}
-*/
 
 class Signal_Det {
     private:
@@ -54,14 +35,14 @@ class Signal_Det {
 	public:
     Signal_Det(ros::NodeHandle *nh) {
 			
-		//image_transport::ImageTransport it(nh);
+	
 		sub = nh->subscribe("/usb_cam/image_raw", 1, &Signal_Det::img0_callback, this);
 		signal_pub = nh->advertise<std_msgs::String>("/signal_info", 10);   
 		config_v3_tiny.net_type = YOLOV3_TINY;
 		config_v3_tiny.detect_thresh = 0.5;
-		config_v3_tiny.file_model_cfg = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/configs/yolov3-tiny-bosch.cfg";
-		config_v3_tiny.file_model_weights = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/configs/yolov3-tiny-bosch_40000.weights";
-		config_v3_tiny.calibration_image_list_file_txt = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/configs/calibration_images.txt";
+		config_v3_tiny.file_model_cfg = "/VISION-TAG/detection-trt/detection-trt/configs/yolov3-tiny-bosch.cfg";
+		config_v3_tiny.file_model_weights = "/VISION-TAG/detection-trt/detection-trt/configs/yolov3-tiny-bosch_40000.weights";
+		config_v3_tiny.calibration_image_list_file_txt = "/VISION-TAG/detection-trt/detection-trt/configs/calibration_images.txt";
 		config_v3_tiny.inference_precison = FP32;
 		detector = new Detector();
 		detector->init(config_v3_tiny);
@@ -69,14 +50,11 @@ class Signal_Det {
 		
     }
     void img0_callback(const sensor_msgs::ImageConstPtr &img_msg) {
-		//std::cout<<"Image callback"<<std::endl;
-		//m_buf.lock();
+	
 		img0_buf.push(img_msg);
 		image0 = this->getImageFromMsg(img0_buf.front());
 		img0_buf.pop();
-		//imshow("image", image0);
-		//waitKey(10);
-		//if (batch_img.empty()){
+		
 		batch_img.push_back(image0);
 		detector->detect(batch_img, batch_res);
 	
@@ -136,11 +114,7 @@ class Signal_Det {
 int main(int argc, char **argv){
 
 	
-	//detector->init(config_v4_tiny);
-	//timer.reset();
-	//detector->detect(batch_img, batch_res);
-	//timer.out("detect");
-		
+	
 	ros::init(argc, argv, "signal_detector");
     ros::NodeHandle nh("~");
     ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
@@ -148,91 +122,7 @@ int main(int argc, char **argv){
 
 	
 	ros::spin();
-	/*
-	Config config_v3;
-	config_v3.net_type = YOLOV3;
-	config_v3.file_model_cfg = "../configs/yolov3.cfg";
-	config_v3.file_model_weights = "../configs/yolov3.weights";
-	config_v3.calibration_image_list_file_txt = "../configs/calibration_images.txt";
-	config_v3.inference_precison =FP32;
-	config_v3.detect_thresh = 0.5;
 
-	Config config_v3_tiny;
-	config_v3_tiny.net_type = YOLOV3_TINY;
-	config_v3_tiny.detect_thresh = 0.5;
-	config_v3_tiny.file_model_cfg = "../configs/yolov3-tiny.cfg";
-	config_v3_tiny.file_model_weights = "../configs/yolov3-tiny.weights";
-	config_v3_tiny.calibration_image_list_file_txt = "../configs/calibration_images.txt";
-	config_v3_tiny.inference_precison = FP32;
-
-	Config config_v4;
-	config_v4.net_type = YOLOV4;
-	config_v4.file_model_cfg = "../configs/yolov4.cfg";
-	config_v4.file_model_weights = "../configs/yolov4.weights";
-	config_v4.calibration_image_list_file_txt = "../configs/calibration_images.txt";
-	config_v4.inference_precison = FP32;
-	config_v4.detect_thresh = 0.5;
-
-	Config config_v4_tiny;
-	config_v4_tiny.net_type = YOLOV4_TINY;
-	config_v4_tiny.detect_thresh = 0.5;
-	config_v4_tiny.file_model_cfg = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/configs/yolov4-tiny.cfg";
-	config_v4_tiny.file_model_weights = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/configs/yolov4-tiny.weights";
-	config_v4_tiny.calibration_image_list_file_txt = "/home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/configs/calibration_images.txt";
-	config_v4_tiny.inference_precison = FP32;
-
-	Config config_v5;
-	config_v5.net_type = YOLOV5;
-	config_v5.detect_thresh = 0.5;
-	config_v5.file_model_cfg = "../configs/yolov5-5.0/yolov5s6.cfg";
-	config_v5.file_model_weights = "../configs/yolov5-5.0/yolov5s6.weights";
-	config_v5.calibration_image_list_file_txt = "../configs/calibration_images.txt";
-	config_v5.inference_precison = FP32;
-	*/
-	// Mat image0 =  imread("../configs/dur6.jpg",  IMREAD_UNCHANGED);
-	// Mat image1 =  imread("../configs/person.jpg",  IMREAD_UNCHANGED);
-
-	//ros::Subscriber sub_img0 = n.subscribe("/mynteye/right/image_raw", 10, img0_callback);
-    
-	//int deviceID = 0;             // 0 = open default camera
-    //int apiID =  CAP_ANY;     
-	//VideoCapture cap("device=/dev/video1");
-	//int cap_id =  CAP_GSTREAMER;
-	//std::cout <<"Here1";
-	//VideoCapture cap("/dev/video0") ;
-	//VideoCapture cap(1);
-
-	// 0 = autodetect default API
-    // open selected camera using selected API
-    //cap.open(deviceID, apiID);
-	//if(!cap.isOpened()){
-	//	std::cout <<"Check camera";
-	//	return -1;
-	//}
-	//for (;;)
-	//{	
-		//if (!img0_buf.empty() )
-		//{
-		//	std::cout<<"ROS Image"<<std::endl;
-		//		image0 = getImageFromMsg(img0_buf.front());
-		//		img0_buf.pop();
-		
-		
-		//prepare batch data
-
-		//Mat frame;
-		//cap.read(frame);
-		//if (frame.empty())
-		//{	std::cout <<"Empty Frame";
-		//	break;
-		//}
-		//std::cout <<"In Loop Image ROS"<<std::endl;
-		//Mat temp0 = image0.clone();
-		
-		// Mat temp1 = image1.clone();
-		//batch_img.push_back(temp1);
-
-		//detect
 		
 		
 	
