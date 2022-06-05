815fe8f31bc8714fe6e24677fcfea00b2a41a8be
Ali Akram <30436017+king-ali@users.noreply.github.com>
2022-06-06 01:09:16 +0500

    Delete detection-trt/build directory

--- detection-trt/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/brain/MYNT-EYE-S-SDK/wrappers/ros/src/yolo-tensorrt/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
